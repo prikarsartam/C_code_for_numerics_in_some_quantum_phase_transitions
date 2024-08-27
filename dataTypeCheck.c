9850dd2d812d92e2186d84ad5e426466c133ddbe
Pritam Sarkar <104849674+prikarsartam@users.noreply.github.com>
2024-08-28 03:03:42 +0530

    Delete dataTypeCheck.c

--- dataTypeCheck.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#include<math.h>
-
-int main() {
-    printf("%ld\n", sizeof(double));      // some compilers print 8
-    printf("%ld\n", sizeof(long double)); // some compilers print 16
-    return 0;
-}
