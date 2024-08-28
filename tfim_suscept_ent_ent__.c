a4bcfcb0ee2797fa1c137ddbade78cc68e21ac13
Pritam Sarkar <104849674+prikarsartam@users.noreply.github.com>
2024-08-28 19:20:51 +0530

    Update tfim_suscept_ent_ent__.c

--- tfim_suscept_ent_ent__.c
+++ tfim_suscept_ent_ent__.c
@@ -6,47 +6,52 @@
 
 
 
-double finite_size_sigma_exp(double h, int n) {
-    const double PI = 2.0 * acos(0.0);
-    double sum = 0.0;
+long double finite_size_sigma_exp(long double h, int n) {
+    const long double PI = 2.0 * acos(0.0);
+    long double sum = 0.0;
 
     for (int m_init = - n/2; m_init <= n/2 - 1 ; m_init++) {
-        double m = m_init + 0.5;
-        double k = 2.0 * PI * (double)m / (double)n;
-        double term = (h - cos(k)) / sqrt(pow(h - cos(k), 2) + pow(sin(k), 2));
+        long double m = (long double)m_init + 0.5;
+        long double k = 2.0 * PI * (long double)m / (long double)n;
+        long double term = (h - cos(k)) / sqrt(pow(h - cos(k), 2) + pow(sin(k), 2));
         sum += term;
     }
-    return sum / (double)n;
+    return sum / (long double)n;
 }
 
-/*
-double Derivative_of_finite_size_sigma_exp(double h, int n) {
-    const double PI = 2.0 * acos(0.0);
-    double sum_n = 0.0;
-    for (int k = -(n-1)/2; k <= (n-1)/2; k++) {
-        double kval = 2.0 * PI * k / n;
-        double term_n = (sin(kval) * sin(kval)) / pow((1.0 + h * h - 2.0 * h * cos(kval)), 1.5);
+
+long double Derivative_of_finite_size_sigma_exp(long double h, int n) {
+    const long double PI = 2.0 * acos(0.0);
+    long double sum_n = 0.0;
+    for (int m_init = - n/2; m_init <= n/2 - 1 ; m_init++) {
+        long double m = (long double)m_init + 0.5;
+        long double kval = 2.0 * PI * m / (long double)n;
+        long double term_n = (sin(kval) * sin(kval)) / pow((1.0 + h * h - 2.0 * h * cos(kval)), 1.5);
         sum_n += term_n;
     }
     return sum_n / n;
 }
-*/
 
 
-double Derivative_of_finite_size_sigma_exp(double h, int n) {
- double delta = 1e-8;
- double delta_mz = (finite_size_sigma_exp(h + delta, n) - finite_size_sigma_exp(h, n));
+
+
+/*
+long double Derivative_of_finite_size_sigma_exp(long double h, int n) {
+ long double delta = 1e-8;
+ long double delta_mz = (finite_size_sigma_exp(h + delta, n) - finite_size_sigma_exp(h, n));
  return delta_mz / delta;
  
 }
+*/
 
 
-double FiniteSizeSusceptibility(double h, int n) {
-    double mz = finite_size_sigma_exp(h, n);
-    double dhmz = Derivative_of_finite_size_sigma_exp(h, n);
+
+long double FiniteSizeSusceptibility(double h, int n) {
+    long double mz = finite_size_sigma_exp(h, n);
+    long double dhmz = Derivative_of_finite_size_sigma_exp(h, n);
     
-    double numerator = dhmz * dhmz;
-    double denominator = 2.0 * (1.0 - mz * mz);
+    long double numerator = dhmz * dhmz;
+    long double denominator = 2.0 * (1.0 - mz * mz);
     
     return numerator / denominator;
 }
@@ -58,8 +63,11 @@ int main(int argc, char *argv[]) {
     }
     
     
-    double h_start = atof(argv[1]);
-    double h_end = atof(argv[2]);
+    double h_initial = atof(argv[1]);
+    double h_final = atof(argv[2]);
+    
+    long double h_start = (long double)h_initial;
+    long double h_end = (long double)h_final;
     
     int samplesize = atoi(argv[3]);
     int n = atoi(argv[4]);
@@ -81,19 +89,19 @@ int main(int argc, char *argv[]) {
     fprintf(file, "h,susceptibility\n");
 
 
-    double h_step = (h_end - h_start) / (samplesize - 1);
+    long double h_step = (h_end - h_start) / ((long double)samplesize - 1);
 
     printf("10 Susceptibility values for equally spaced h values:\n");
     printf("h,susceptibility\n");
 
     for (int i = 0; i < samplesize; i++) {
-        double h = h_start + i * h_step;
-        double suscept = FiniteSizeSusceptibility(h, n);
-        fprintf(file, "%.15e,%.15e\n", h, suscept);
+        long double h = h_start + i * h_step;
+        long double suscept = FiniteSizeSusceptibility(h, n);
+        fprintf(file, "%.30Lf,%.30Lf\n", h, suscept);
         
         // Print 10 equally spaced values
         if (i % (samplesize / 10) == 0 || i == samplesize - 1) {
-            printf("%.15e,%.15e\n", h, suscept);
+            printf("%.30Lf,%.30Lf\n", h, suscept);
         }
     }
 
