6d7fe5aea13596df08b5303b36fc72ff30637a4a
Pritam Sarkar <104849674+prikarsartam@users.noreply.github.com>
2024-08-28 19:21:26 +0530

    Update tfim_suscept_ent_ent_peak_scaling__.c

--- tfim_suscept_ent_ent_peak_scaling__.c
+++ tfim_suscept_ent_ent_peak_scaling__.c
@@ -5,38 +5,38 @@
 
 
 #define GOLDEN_RATIO 1.6180339887498948482045868
-#define TOLERANCE 1e-18
-#define MAX_ITERATIONS 10000
+#define TOLERANCE 1e-20
+#define MAX_ITERATIONS 100000
 
 
 
 
 
-double finite_size_sigma_exp(double h, double gamma, int n) {
-    const double PI = 2.0 * acos(0.0);
-    double sum = 0.0;
+long double finite_size_sigma_exp(long double h, long double gamma, int n) {
+    const long double PI = 2.0 * acos(0.0);
+    long double sum = 0.0;
 
     for (int k_init = - n/2; k_init <= n/2 - 1 ; k_init++) {
-        double k = k_init + 0.5;
-        double kval = 2.0 * PI * k / (double)n;
-        double term = (h - cos(kval)) / sqrt(pow(h - cos(kval), 2) + pow(gamma * sin(kval), 2));
+        long double k = (long double)k_init + 0.5;
+        double kval = 2.0 * PI * k / (long double)n;
+        long double term = (h - cos(kval)) / sqrt(pow(h - cos(kval), 2) + pow(gamma * sin(kval), 2));
         sum += term;
     }
-    return sum / (double)n;
+    return sum / (long double)n;
 }
 
 
 
-double Derivative_of_finite_size_sigma_exp(double h, double gamma, int n) {
-    const double PI = 2.0 * acos(0.0);
-    double sum_n = 0.0;
+double Derivative_of_finite_size_sigma_exp(long double h, long double gamma, int n) {
+    const long double PI = 2.0 * acos(0.0);
+    long double sum_n = 0.0;
     for (int k_init = - n/2; k_init <= n/2 - 1 ; k_init++) {
-        double k = k_init + 0.5;
-        double kval = 2.0 * PI * k / (double)n;
-        double term_n = (pow(gamma * sin(kval) , 2)) / pow( ( pow(h - cos(kval), 2) + pow(gamma * sin(kval), 2) ), 1.5);
+        long double k = k_init + 0.5;
+        long double kval = 2.0 * PI * k / (long double)n;
+        long double term_n = (pow(gamma * sin(kval) , 2)) / pow( ( pow(h - cos(kval), 2) + pow(gamma * sin(kval), 2) ), 1.5);
         sum_n += term_n;
     }
-    return sum_n / (double)n;
+    return sum_n / (long double)n;
 }
 
 
@@ -52,46 +52,46 @@ double Derivative_of_finite_size_sigma_exp(double h, int n) {
 */
 
 
-double calculate_g11(double h, double gamma, int n) {
+long double calculate_g11(long double h, long double gamma, int n) {
 
     
-    double mz = finite_size_sigma_exp(h, gamma, n);
-    double dhmz = Derivative_of_finite_size_sigma_exp(h, gamma, n);
+    long double mz = finite_size_sigma_exp(h, gamma, n);
+    long double dhmz = Derivative_of_finite_size_sigma_exp(h, gamma, n);
     
-    double numerator =  pow(dhmz, 2) ;
-    double denominator =  2 * (1 - pow(mz, 2));
+    long double numerator =  pow(dhmz, 2) ;
+    long double denominator =  2 * (1 - pow(mz, 2));
     
     return numerator/denominator ;
 }
 
 
-double parabolic_interpolation(double (*f)(double, double, int), double x0, double x1, double x2, double gamma, int n) {
-    double f0 = f(x0, gamma, n);
-    double f1 = f(x1, gamma, n);
-    double f2 = f(x2, gamma, n);
+long double parabolic_interpolation(long double (*f)(long double, long double, int), long double x0, long double x1, long double x2, long double gamma, int n) {
+    long double f0 = f(x0, gamma, n);
+    long double f1 = f(x1, gamma, n);
+    long double f2 = f(x2, gamma, n);
     
-    double x = x1 - 0.5 * ((x1 - x0) * (x1 - x0) * (f1 - f2) - (x1 - x2) * (x1 - x2) * (f1 - f0)) /
+    long double x = x1 - 0.5 * ((x1 - x0) * (x1 - x0) * (f1 - f2) - (x1 - x2) * (x1 - x2) * (f1 - f0)) /
                           ((x1 - x0) * (f1 - f2) - (x1 - x2) * (f1 - f0));
     
     return x;
 }
 
-double optimize_g11(int n) {
-    double gamma = 1.0;
-    double a = 1- 1/((double) n), b = 1 + 1/((double) n); // Assuming the peak is near h=1
-    double x0 = a, x3 = b;
+long double optimize_g11(int n) {
+    long double gamma = 1.0;
+    long double a = 1- 1/((long double) n), b = 1 + 1/((long double) n); // Assuming the peak is near h=1
+    long double x0 = a, x3 = b;
 
 /*
     double x1 = x0 + (x3 - x0) / GOLDEN_RATIO;
     double x2 = x3 - (x3 - x0) / GOLDEN_RATIO;
 */
     
-    double x1 = x0 + (x3 - x0)/5.0;
-    double x2 = x3 - (x3 - x0)/5.0;
+    long double x1 = x0 + (x3 - x0)/5.0;
+    long double x2 = x3 - (x3 - x0)/5.0;
     
     
     for (int i = 0; i < MAX_ITERATIONS; i++) {
-        double x_new = parabolic_interpolation(calculate_g11, x0, x1, x3, gamma, n);
+        long double x_new = parabolic_interpolation(calculate_g11, x0, x1, x3, gamma, n);
         
         if (fabs(x_new - x1) < TOLERANCE) {
             return x_new;
@@ -119,8 +119,8 @@ double optimize_g11(int n) {
 }
 
 
-double tfim_size_vs_peak_distance_from_criticality(int n) {
-    double h_critical = optimize_g11(n);
+long double tfim_size_vs_peak_distance_from_criticality(int n) {
+    long double h_critical = optimize_g11(n);
     return fabs(1.0 - h_critical);
 }
 
@@ -137,10 +137,10 @@ void calculate_and_store_data(const int* size_list, int size_count, const char* 
     
     for (int i = 0; i < size_count; i++) {
         int size = size_list[i];
-        double distance = tfim_size_vs_peak_distance_from_criticality(size);
-        fprintf(file, "%.17g,%.17g\n", 1/(double)size, distance); // %.17g for exact double precision
+        long double distance = tfim_size_vs_peak_distance_from_criticality(size);
+        fprintf(file, "%.30Lf,%.30Lf\n", 1/(long double)size, distance); 
 
-        printf("| h_c(%d) - h_c(thermo) |  = %.17e\n", size, distance);
+        printf("| h_c(%d) - h_c(thermo) |  = %.30Lf\n", size, distance);
     }
     
     fclose(file);
@@ -153,16 +153,19 @@ int main() {
     int size_list[] = { 4, 8, 12, 16, 24, 36, 48, 56, 62, 76, 90, 96, 102, 150, 286, 378, 500, 650, 748, 800, 1000, 2000, 2750, 3500, 5000, 7000, 8000, 9000, 10000, 11500, 12000, 13500, 15000, 17500, 20000, 25000, 50000, 65000, 77800, 85000, 94200, 100000};
     
 */
+
 int size_list[] = {
     4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 
     54, 56, 58, 60, 62, 64, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100, 102, 
     110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 250, 286, 300, 350, 378, 400, 450, 500, 550, 650, 
-    748, 800, 850, 900, 950, 1000, 1250, 1400, 1650, 1800, 2000, 3000, 4000, 5000, 7500, 10000, 20000, 50000, 100000
+    748, 800, 850, 900, 950, 1000, 1250, 1400, 1650, 1800, 2000, 3000, 4000, 5000, 7500, 10000, 20000, 36000, 50000, 75000, 100000,150000, 200000, 250000, 500000, 750000,1000000
 };
+
+
     
     int size_count = sizeof(size_list) / sizeof(size_list[0]);
     
-    calculate_and_store_data(size_list, size_count, "tfim_suscept_peak_data__28072024_0300.csv");
+    calculate_and_store_data(size_list, size_count, "tfim_suscept_peak_data__28072024_1650__inLongDouble.csv");
     
     return 0;
 }
